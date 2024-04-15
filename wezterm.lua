2504feb757e7a9daf14fd92edb076a89e99ef686
Rodrigo Pinheiro Matias <rodrigopmatias@gmail.com>
2024-04-15 08:27:47 -0300

    feat: foi configurado a diminsão da inicialização

--- wezterm.lua
+++ wezterm.lua
@@ -1,15 +1,19 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Ocean (dark) (terminal.sexy)'
-config.font = wezterm.font { family = 'LigaConsolas Nerd Font', scale = 1.0 }
-config.font_size = 12.0
-config.freetype_load_target = 'Light'
+config.color_scheme = "Dracula (Gogh)"
+config.window_background_opacity = 0.9
+
+config.font = wezterm.font({ family = "LigaConsolas Nerd Font", scale = 1.0 })
+config.font_size = 11.5
+config.freetype_load_target = "Light"
 config.line_height = 1.8
-config.window_padding = { top = 0, left = 5, right = 5, bottom = 0 }
+config.window_padding = { top = 7, left = 7, right = 7, bottom = 0 }
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
--- config.window_decorations = "RESIZE"
+
+config.initial_cols = 230
+config.initial_rows = 35
 
 return config
